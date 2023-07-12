10c8cdb39344d67b7eaa055e95f36ee037af74d3
Paul Whitby <paulwhitby@btconnect.com>
2023-07-12 18:24:40 +0100

    Refactor into functions

--- nvc_database_create.py
+++ nvc_database_create.py
@@ -1,9 +1,11 @@
 import sqlite3
 
-con = sqlite3.connect("nvc.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE community(key, community, community_code, community_name)")
-cur.execute("CREATE TABLE species(species_name)")
-cur.execute("CREATE TABLE community_species(community_code, species, abundance, frequency)")
-con.commit()
-con.close()
+def create_database():
+    con = sqlite3.connect("nvc.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE community(key, community, community_code, community_name)")
+    cur.execute("CREATE TABLE species(species_name)")
+    cur.execute("CREATE TABLE community_species(community_code, species, abundance, frequency)")
+    con.commit()
+    con.close()
+
--- nvc_database_load.py
+++ nvc_database_load.py
@@ -1,23 +1,24 @@
 import sqlite3
 import csv
 
-# open database
-con = sqlite3.connect("nvc.db")
+def database_load():
+    # open database
+    con = sqlite3.connect("nvc.db")
 
-# create cursor
-cur = con.cursor()
+    # create cursor
+    cur = con.cursor()
 
-# open community csv
-with open('community.csv') as community_file:
-    reader = csv.reader(community_file)
-    # iterate over community file
-    #    read row
-    for row in reader:
-        print(', '.join(row))
-        #    write row to database
-        res = cur.execute("INSERT INTO community(?, ?, ?, ?)", row)
-        con.commit()
+    # open community csv
+    with open('community.csv') as community_file:
+        reader = csv.reader(community_file)
+        # iterate over community file
+        #    read row
+        for row in reader:
+            print(', '.join(row))
+            #    write row to database
+            res = cur.execute("INSERT INTO community(?, ?, ?, ?)", row)
+            con.commit()
 
 
-# do same for other 2 files / tables
+    # do same for other 2 files / tables
 
