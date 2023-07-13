fb1362898b31ac23aa445917b0ebce90c49e1977
Paul Whitby <paulwhitby@btconnect.com>
2023-07-13 22:19:55 +0100

    multiple utility queries; good baseline database

--- query_join.py
+++ query_join.py
@@ -44,7 +44,24 @@ SELECT
 FROM 
   community_species 
   LEFT JOIN communities ON community_species.community_key = communities.community_key
-  INNER JOIN species ON community_species.species_key = species.species_key;
+  LEFT JOIN species ON community_species.species_key = species.species_key;
+"""
+
+list_three_table_join_where = """
+SELECT
+  community_species.community_key AS commkey,
+  communities.community_name AS commname,
+  communities.community_level_code AS levelcode,
+  communities.community_code AS commcode,
+  community_species.species_key AS speckey,
+  species.species_name AS specname,
+  community_species.abundance AS abund,
+  community_species.frequency AS freq
+FROM 
+  community_species 
+  LEFT JOIN communities ON community_species.community_key = communities.community_key
+  LEFT JOIN species ON community_species.species_key = species.species_key
+WHERE commcode = 'H1e';
 """
 
 list_communities_species_join = """
@@ -98,9 +115,9 @@ def query_join():
     con = sqlite3.connect("nvc.db")
     cur = con.cursor()
 
-    for row in cur.execute(list_community_species):
-        print(', '.join(row))
-        time.sleep(0.2)
+    for row in cur.execute(list_three_table_join_where):
+        print(' | '.join(row))
+        # time.sleep(0.2)
     
     con.close()
 
