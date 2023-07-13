285e1d7e191606def03750e5eb915b9be8008265
Paul Whitby <paulwhitby@btconnect.com>
2023-07-13 19:14:14 +0100

    update database create to reflect new schemas

--- nvc_database_create.py
+++ nvc_database_create.py
@@ -3,9 +3,9 @@ import sqlite3
 def create_database():
     con = sqlite3.connect("nvc.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE community(key, community, community_code, community_name)")
-    cur.execute("CREATE TABLE species(species_name)")
-    cur.execute("CREATE TABLE community_species(community_code, species, abundance, frequency)")
+    cur.execute("CREATE TABLE communities(community_key, community_code, community_name, community_level_code)")
+    cur.execute("CREATE TABLE species(species_key, species_name)")
+    cur.execute("CREATE TABLE community_species(community_key, species_key, abundance, frequency, special)")
     con.commit()
     con.close()
 
