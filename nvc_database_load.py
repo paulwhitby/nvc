3a3662b86636129e13caeb2c420758a92c31c81a
Paul Whitby <paulwhitby@btconnect.com>
2023-07-13 19:21:23 +0100

    code to load each of 3 database tables from csv

--- nvc_database_load.py
+++ nvc_database_load.py
@@ -1,6 +1,10 @@
 import sqlite3
 import csv
 
+# communities(community_key, community_code, community_name, community_level_code)
+# species(species_key, species_name)
+# community_species(community_key, species_key, abundance, frequency, special)
+
 def database_load():
     # open database
     con = sqlite3.connect("nvc.db")
@@ -8,17 +12,41 @@ def database_load():
     # create cursor
     cur = con.cursor()
 
-    # open community csv
-    with open('community.csv') as community_file:
+    # open community_csv
+    with open('community_csv.csv') as community_file:
         reader = csv.reader(community_file)
         # iterate over community file
         #    read row
         for row in reader:
-            print(', '.join(row))
+            # print(', '.join(row))
             #    write row to database
-            res = cur.execute("INSERT INTO community(?, ?, ?, ?)", row)
+            res = cur.execute("INSERT INTO communities(?, ?, ?, ?)", row)
             con.commit()
 
 
-    # do same for other 2 files / tables
+    # open species_csv
+    with open('species_csv.csv') as species_file:
+        reader = csv.reader(species_file)
+        # iterate over community file
+        #    read row
+        for row in reader:
+            # print(', '.join(row))
+            #    write row to database
+            res = cur.execute("INSERT INTO community_species(?, ?)", row)
+            con.commit()
+
+
+    # open community_species_csv
+    with open('community_species_csv.csv') as community_species_file:
+        reader = csv.reader(community_species_file)
+        # iterate over community file
+        #    read row
+        for row in reader:
+            # print(', '.join(row))
+            #    write row to database
+            res = cur.execute("INSERT INTO species(?, ?, ?, ?, ?)", row)
+            con.commit()
+    
+    con.commit()
+    con.close()
 
