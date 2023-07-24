b6d52e850a26c867b15a05e229ba1259da08b88e
Paul Whitby <paulwhitby@btconnect.com>
2023-07-24 20:10:16 +0100

    refactored to factor out common database load

--- nvc_database_load.py
+++ nvc_database_load.py
@@ -5,7 +5,30 @@ import csv
 # species(species_key, species_name)
 # community_species(community_key, species_key, abundance, frequency, special)
 
-def load_database():
+
+def load_table(database_name, csv_file_name, query_string, check_string):
+    # open database
+    con = sqlite3.connect(database_name)
+
+    # create cursor
+    cur = con.cursor()
+
+    # open csv
+    print("Loading ", csv_file_name)
+    with open(csv_file_name) as csv_file:
+        reader = csv.reader(csv_file)
+        # iterate over community file
+        for row in reader:
+            # print(', '.join(row))
+            cur.execute(query_string, row)
+            con.commit()
+    for row in cur.execute(check_string):
+        print(row, "records")
+    
+    con.close()
+
+
+def load_database_old():
     # open database
     con = sqlite3.connect("nvc.db")
 
@@ -55,3 +78,8 @@ def load_database():
     con.commit()
     con.close()
 
+
+def load_database():
+    load_table("nvc.db", "community_csv.csv", "INSERT INTO communities VALUES(?, ?, ?, ?)", "select count(*) from communities")
+    load_table("nvc.db", "Loading species_csv", "INSERT INTO species VALUES(?, ?)", "select count(*) from species")
+    load_table("nvc.db", "Loading community_species_csv", "INSERT INTO community_species VALUES(?, ?, ?, ?, ?)", "select count(*) from community_species")
