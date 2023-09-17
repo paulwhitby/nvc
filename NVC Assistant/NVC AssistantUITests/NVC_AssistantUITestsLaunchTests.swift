33cd0739d93f4ef9bff8ad55ce786e634b6487ef
Paul Whitby <paulwhitby@btconnect.com>
2023-09-17 12:50:31 +0100

    Added succession_csv structure and started iOS app

---
+++ NVC Assistant/NVC AssistantUITests/NVC_AssistantUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NVC_AssistantUITestsLaunchTests.swift
+//  NVC AssistantUITests
+//
+//  Created by Paul Whitby MBP15 on 16/08/2023.
+//
+
+import XCTest
+
+final class NVC_AssistantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
