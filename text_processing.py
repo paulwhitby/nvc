2191be9d33d2459515717352528368b1fef16fca
Paul Whitby <paulwhitby@btconnect.com>
2024-04-19 10:58:27 +0100

    clean up texts more, add more phrases for succession reasons

--- text_processing.py
+++ text_processing.py
@@ -57,13 +57,13 @@ ALTERNATIVE_NAMES = {
 }
 
 # reasons why succession might take place. To be extended
-SUCCESSION_DRIVERS = {'grazingstopped':["cessation of grazing", "abandonment of grazing", "lack of grazing"],
-                      'grazing': ["grazing", "intensive grazing", "heavily grazed", "grazed", "introduction of grazing", "rabbit-grazing"],
+SUCCESSION_DRIVERS = {'grazingstopped':["cessation of grazing", "abandonment of grazing", "grazing is abandoned", "lack of grazing", "grazing is restricted", "grazing is withdrawn", "demise of rabbits", "reduction in rabbit-grazing", "ungrazed"],
+                      'grazing': ["intensive grazing", "heavily grazed", "grazed", "introduction of grazing", "rabbit-grazing", "grazing"],
                       'ploughingstopped': ["abandonment of ploughing", "cessation of ploughing"],
                       'ploughingstarted': ["introduction of ploughing", "ploughing"],
                       'wetting': ["wetting"],
                       'drying': ["drying"],
-                      'fire': ["fire", "wildfire"],
+                      'fire': ["fire", "wildfire", "burning", "burns"],
                       'mowingstopped': ["abandonment of mowing", "cessation of mowing"],
                       'abandonment': ["abandonment", "abandon", "abandoned"],
                       'myxomatosis': ["myxomatosis"],
