d0cdbe819b3bdd079d92f260232e3ffbe08aeca5
Paul Whitby <paulwhitby@btconnect.com>
2025-12-05 13:45:45 +0000

    starting to add context - history

--- ai-experiments/streamlit_nvc_chatbot.py
+++ ai-experiments/streamlit_nvc_chatbot.py
@@ -17,6 +17,10 @@ from langchain_classic.chains import create_retrieval_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 import streamlit as st
+# Add these to your imports
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
 
 
 # Set your API Key securely
